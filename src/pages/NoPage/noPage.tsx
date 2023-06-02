33d3cd257730260b78ba74deb0df6d637e106c30
Sebastiaan den Hertog <sebastiaan.denhertog@maxem.energy>
2023-06-02 11:56:55 +0200

    feat: :sparkles: multiple pages you can interact with.

---
+++ src/pages/NoPage/noPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../Layout/Layout";
+
+const NoPage = () => {
+  return (
+    <main>
+      <h1>404</h1>
+      <Layout />
+    </main>
+  )
+};
+
+export default NoPage;
