f2f13e5b4a203b8848eaa8a898182c7663f1ba80
Sebastiaan den Hertog <sebastiaan.denhertog@maxem.energy>
2023-06-22 22:44:07 +0200

    refactor: start new react ts project

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+const Login = () => {
+    return (
+        <main>
+            <h1>Login</h1>
+            <Layout />
+        </main>
+    )
+};
+
+export default Login;
---
+++ src/pages/Settings/Settings.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../Layout/Layout";
+
+const Settings = () => {
+  return (
+    <main>
+      <h1>Settings</h1>
+      <Layout />
+    </main>
+  )
+};
+
+export default Settings;
