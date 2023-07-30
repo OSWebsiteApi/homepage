cdfd948b7e050a00cd75e7c3b3cd36b0fbead50d
anonymous0230 <sebastiaan.denhertog@gmail.com>
2023-07-30 13:54:24 +0200

    reworkt the forms so we can use them (not working yet)

--- src/Elements/Froms.tsx
+++ src/Elements/Froms.tsx
@@ -1,32 +1,114 @@
 import React from "react";
+import {useState} from "react";
+
+
 
 export function LoginFrom() {
+    const [inputs, setInputs] = useState({
+        email: undefined,
+        Password: undefined
+    });
+
+    const handleChange = (event:any) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit  = (event:any) => {
+        event.preventDefault();
+        console.log(inputs)
+    }
+
     return (
-        <form>
-            <label htmlFor="e-mail">E-mail:</label><br/>
-            <input type="email" id="e-mail" name="e-mail" value="Excemple@oFyreblox.com"/><br/>
-            <label htmlFor="Password">Password:</label><br/>
-            <input type="text" id="Password" name="Password" value="Doe"/><br/><br/>
-            <input type="submit" value="Submit"/>
+        <form onSubmit={handleSubmit}>
+            <label> E-mail:
+                <br />
+            <input
+                type="email"
+                name="emial"
+                value={inputs.email || ""}
+                onChange={handleChange}
+            />
+            </label>
+            <br/>
+            <label>Password:
+                <br />
+            <input
+                type="password"
+                name="Password"
+                value={inputs.Password || ""}
+                onChange={handleChange}
+            />
+            </label>
+            <br/><br/>
+            <input type="submit" />
         </form>
     )
 }
 
-
 export function RegisterForm() {
+    const [inputs, setInputs] = useState({
+        fname: undefined,
+        lname: undefined,
+        email: undefined,
+        Password: undefined,
+        RPassword: undefined,
+
+    });
+
+    const handleChange = (event:any) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit  = (event:any) => {
+        event.preventDefault();
+        console.log(inputs)
+    }
+
     return (
-        <form>
-            <label htmlFor="fname">First name:</label><br/>
-            <input type="text" id="fname" name="fname" value="John"/><br/>
-            <label htmlFor="lname">Last name:</label><br/>
-            <input type="text" id="lname" name="lname" value="Doe"/><br/>
-            <label htmlFor="e-mail">E-mail:</label><br/>
-            <input type="email" id="e-mail" name="e-mail" value="Excemple@oFyreblox.com"/><br/>
-            <label htmlFor="Password">Password:</label><br/>
-            <input type="password" id="Password" name="Password" value="Doe"/><br/>
-            <label htmlFor="RPassword">Retype Password:</label><br/>
-            <input type="password" id="RPassword" name="RPassword" value="Doe"/><br/><br/>
-            <input type="submit" value="Submit"/>
+        <form onSubmit={handleSubmit}>
+            <label>First name:
+            <input
+                type="text"
+                name="fname"
+                value={inputs.fname || ""}
+                onChange={handleChange}
+            /></label><br/>
+            <label>Last name:
+            <input
+                type="text"
+                name="lname"
+                value={inputs.lname || ""}
+                onChange={handleChange}
+            />
+            </label><br/>
+            <label>E-mail:
+            <input
+                type="email"
+                name="email"
+                value={inputs.email || ""}
+                onChange={handleChange}
+            />
+            </label><br/>
+            <label>Password:
+            <input
+                type="password"
+                name="Password"
+                value={inputs.Password || ""}
+                onChange={handleChange}
+            />
+            </label><br/>
+            <label>Retype Password:
+            <input
+                type="password"
+                name="RPassword"
+                value={inputs.RPassword || ""}
+                onChange={handleChange}
+            /></label><br/>
+            <input type="submit"/>
         </form>
     )
 }
