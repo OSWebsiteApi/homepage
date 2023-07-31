c27413bcc14e8312d6dc6c3de7d50d6928c07875
anonymous0230 <sebastiaan.denhertog@gmail.com>
2023-07-31 16:21:24 +0200

    fix bug no http url to api

--- src/Comminucations/API.tsx
+++ src/Comminucations/API.tsx
@@ -3,14 +3,14 @@ import * as path from "path";
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 export async function apiLoginPostRequest(content: any, urlpath: string) {
-    const urlPlusPath = process.env.API_MYSQL_URI || "stagingapi.game-x.nl" + urlpath
+    const urlPlusPath = process.env.API_URL || "stagingapi.game-x.nl" + urlpath
     const response = await fetch(urlPlusPath, {
         method: 'POST',
         body: JSON.stringify({
             email: content.Email,
             password: content.Password
         }),
-        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        headers: {'Content-Type': 'application/json; charset=UTF-8'}
     });
     if (!response.ok) { /* Handle */
     }
@@ -20,7 +20,6 @@ export async function apiLoginPostRequest(content: any, urlpath: string) {
         // parse as needed, e.g. reading directly, or
         console.log(response.body)
         return response.body
-
     }
 }
 
