31278e0ba39bbb4c68d498ac316ff68d99abc9bc
Sebastiaan <Sebastiaan.denhertog@gmail.com>
2023-10-18 21:49:18 +0200

    feat: js file for getting the data form a md file

--- src/scripts/progressbar_script.js
+++ src/scripts/progressbar_script.js
@@ -1,8 +1,18 @@
-fetch('')
-    .then(response => response.text())
-    .then(mdContent => {
-        // Process the MD content here
+// Replace these with your actual GitLab API endpoint and token
+const apiUrl = 'https://gitlab.com/api/v4/groups/opensourceecommerce1/issues'
+const authToken = 'YOUR_PERSONAL_ACCESS_TOKEN';
+
+fetch(apiUrl, {
+    headers: {
+        'Private-Token': authToken,
+    },
+})
+    .then(response => response.json())
+    .then(issues => {
+        issues.forEach(issue => {
+            if (issue.state === 'closed') {
+                // Mark this issue as "done" or display it as such
+            }
+        });
     })
-    .catch(error => {
-        console.error('Error fetching MD file: ', error);
-    });
+    .catch(error => console.error('Error fetching GitLab issues:', error));
